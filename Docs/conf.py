cde5328ca10e29ac8d179d95e1806a63daf9b5e6
Pierre Jacques <pierre.jac1@proton.me>
2023-12-29 16:30:49 +0100

    Change html theme

--- Docs/conf.py
+++ Docs/conf.py
@@ -51,7 +51,7 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'default' #There is a mistake with sphinx_rtd_theme, cannot be installed even by adding it in requirements and extensions = []. Issue discussed here: https://github.com/readthedocs/sphinx_rtd_theme/issues/1463
 numfig = True # Add figure numbering
 numtab = True # Add table numbering
 
