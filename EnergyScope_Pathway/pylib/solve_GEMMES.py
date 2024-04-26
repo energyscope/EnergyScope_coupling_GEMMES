502ce38b4678a91df9cad38f3393b92b251af9d3
Pierre Jacques <pierre.jac1@proton.me>
2024-04-26 17:11:19 +0200

    Add functions for running GEMMES in python

---
+++ EnergyScope_Pathway/pylib/solve_GEMMES.py
@@ -0,0 +1,73 @@
+import numpy as np
+import pandas as pd
+    
+def solveGEMMES(solvePy=None, time=None, y0=None, parms=None, samplesExogVar=None, solver="dopri", 
+          atol=1e-4, rtol=0, fac=0.85, facMin=0.1, facMax=4, nStepMax=100, hInit = 0.01, hMin=0.0001, hMax=0.5):
+    
+    #INITIALIZATION
+    if time is None:
+        nt = solvePy.nt()
+        tInit = solvePy.tInit()
+        tEnd = solvePy.tEnd()
+    else: 
+        nt = time.size
+        tInit = time[0]
+        tEnd=time[time.size-1]
+
+    if hMax>(tEnd-tInit)/(nt-1):
+        hMax = (tEnd-tInit)/(nt-1)
+    
+
+    if y0 is None:
+        y0 = solvePy.yInit()
+    if parms is None:
+        parms = solvePy.parms()
+    if samplesExogVar is None:
+        samplesExogVarCpp = solvePy.samplesExogVar()
+        nSamplesVarExogVarCpp = solvePy.nSamplesVarExogVar()
+        nVarExogVarCpp = solvePy.nVarExogVar()
+    else:
+        samplesExogVarCpp = [0.0]*samplesExogVar.size
+        nSamplesVarExogVarCpp = [samplesExogVar.shape[0]]*samplesExogVar.shape[1]
+        nVarExogVarCpp = samplesExogVar.shape[1]
+        for i in range(nVarExogVarCpp):
+            for j in range(nSamplesVarExogVarCpp[0]):
+                samplesExogVarCpp[i*samplesExogVar.shape[0] + j] = samplesExogVar[i][j]
+        
+    nV = solvePy.nV()
+    nIV = solvePy.nIV()
+    returnRK4 = solvePy.returnType()
+    
+    # CALL C++ CODE FOR RK4
+    if solver=="dopri":
+        outCpp = solvePy.dopri(nt, tInit, tEnd, nV, nIV, y0, parms, samplesExogVarCpp, nSamplesVarExogVarCpp, nVarExogVarCpp,
+                               atol, rtol, fac, facMin, facMax, nStepMax, hInit, hMin, hMax)    
+    elif solver=="RK4Fixed":
+        outCpp = solvePy.RK4Fixed(nt, tInit, tEnd, nV, nIV, y0, parms, samplesExogVarCpp, nSamplesVarExogVarCpp, nVarExogVarCpp)    
+    else: 
+        outCpp = solvePy.euler(nt, tInit, tEnd, nV, nIV, y0, parms, samplesExogVarCpp, nSamplesVarExogVarCpp, nVarExogVarCpp)    
+    
+    # FORMAT OUTPTUT
+    varNames = solvePy.varNames()
+    intermediateVarNames = solvePy.intermediateVarNames()
+    derivVarNames = [varName+ "Dot" for varName in varNames]        
+    
+    
+    if(returnRK4 == 3):
+        outSize = 2*nV+nIV
+        outNames = varNames + derivVarNames + intermediateVarNames
+    elif (returnRK4 == 2):
+        outSize = nV+nIV
+        outNames = varNames + intermediateVarNames
+    elif (returnRK4 == 1):
+        outSize = 2*nV
+        outNames = varNames + derivVarNames
+    else:
+        outSize = nV
+        outNames =varNames
+
+    outUnNamed = np.zeros((nt,outSize))
+    for i in range(nt):
+        outUnNamed[i,:] = outCpp[(i*(outSize)):(i*(outSize)+(outSize))]
+    out = pd.DataFrame(outUnNamed, columns=outNames, index=time)
+    return out
---
+++ EnergyScope_Pathway/src/run_GEMMES.py
@@ -0,0 +1,46 @@
+import os, sys
+from os import system
+from pathlib import Path
+import webbrowser
+import time
+import multiprocessing as mp
+import pickle as pkl
+import pandas as pd
+import numpy as np
+import amplpy as apy
+from solve_GEMMES import solveGEMMES
+from collections import namedtuple
+import cppimport
+
+curr_dir = Path(os.path.dirname(__file__))
+
+pymodPath = os.path.abspath(os.path.join(curr_dir.parent,'pylib'))
+GEMMES_path = '/home/piejacques/Bureau/ColombiaEnergyScope'
+Cpp_path = GEMMES_path + '/SourceCode/cppCode'
+sys.path.insert(0, pymodPath)
+sys.path.insert(0, Cpp_path)
+sys.path.insert(0, Cpp_path + "/src")
+
+cppimport.settings['force_rebuild'] = True
+solvePy = cppimport.imp('functionsForPy')
+
+## Change parameters values in GEMMES
+# Build default parms vector in python importing data from C++
+parmsNames = solvePy.parmsNames()
+# Declare a structure that stores parameters values and their names
+namedParms = namedtuple("namedParms", parmsNames)
+# build named parms vector using the parmsNamed structure and loading parms values from C++
+newParms = namedParms(*solvePy.parms())
+newParms = newParms._replace(sigmaxnpNew=1.2)
+newParms = newParms._replace(kappa03=0.03526333*1.05)
+newParms = newParms._replace(tauf=0.1859935*1.02)
+newParms = newParms._replace(taub=0.1222547*1.02)
+newParms = newParms._replace(tauw=0.08986282*1.02)
+newParms = newParms._replace(tauvat=0.1089564*1.02)
+newParms = newParms._replace(fi3=0.45)
+newParms = newParms._replace(sigmaxnSpeed=0.48)
+newParms = newParms._replace(reducXrO=0)
+
+out = solveGEMMES(solvePy=solvePy, parms=newParms, solver="dopri", atol=1e-4, rtol=0, fac=0.85, facMin=0.1, facMax=4, nStepMax=300, hInit=0.025, hMin=0.025/100, hMax=0.2)
+
+out.plot(y=["ip"], use_index=True)
