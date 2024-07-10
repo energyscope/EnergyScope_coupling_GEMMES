357b4d9ebb5ce1a97f2ba1187ce074d6a83e183b
Pierre Jacques <pierre.jac1@proton.me>
2024-07-10 18:00:15 +0200

    Add file run_energyScope_pathway_only.py

--- EnergyScope_Pathway/src/run_main_coupled_model.py
+++ EnergyScope_Pathway/src/run_main_coupled_model.py
@@ -11,7 +11,7 @@ EnergyScope_granularity = 'MO'
 nbr_tds = 12
 
 def main():
-    plot_EnergyScope = False  
+    plot_EnergyScope = True  
     csv_EnergyScope  = True
     plot_GEMMES = True
     csv_GEMMES = False
